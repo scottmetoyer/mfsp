0a5a646b30a16661f5008e19ad00a549f72b4333
Scott Metoyer <scott.metoyer@gmail.com>
2020-10-23 07:35:13 -0700

    Added console app

---
+++ app/assets/global.js
@@ -0,0 +1,9 @@
+function handleRender() {
+  let seed = document.getElementById("txtSeed").value;
+
+
+}
+
+document.querySelector('#btnRender').addEventListener('click', () => {
+  handleRender();
+})
