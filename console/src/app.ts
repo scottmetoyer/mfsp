57fd596315fb319a03dc263ca8ed017a886616d4
Scott Metoyer <scott.metoyer@gmail.com>
2020-11-03 11:37:51 -0800

    Restructured node project

--- console/src/app.ts
+++ console/src/app.ts
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Ready for input > ", function(input) {
+rl.question("Ready for input > ", function(input: string) {
   console.log(`Got some input: ${input}`);
 
   // Test midi creation
@@ -15,14 +15,14 @@ rl.question("Ready for input > ", function(input) {
   var track = new Midi.Track();
   file.addTrack(track);
 
-  track.addNote(0, 'c4', 64);
+  track.addNote(0, 'c4', 32);
   track.addNote(0, 'd4', 64);
-  track.addNote(0, 'e4', 64);
-  track.addNote(0, 'f4', 64);
-  track.addNote(0, 'g4', 64);
-  track.addNote(0, 'a4', 64);
+  track.addNote(0, 'e4', 1024);
+  track.addNote(0, 'f4', 32);
+  track.addNote(0, 'g4', 16);
+  track.addNote(0, 'a4', 16);
   track.addNote(0, 'b4', 64);
-  track.addNote(0, 'c5', 64);
+  track.addNote(0, 'c5', 128);
 
   fs.writeFileSync('test.mid', file.toBytes(), 'binary');
   rl.close();
